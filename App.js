7ce0c15cd8be7a0ad294ca426acda67b317449ab
sjordan20 <jordanstephen949@gmail.com>
2020-07-20 13:29:03 -0600

    created todo app

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import Header from './components/header'
+import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'Learn React Native', key: '1' },
+    { text: 'Eat Lunch', key: '2' },
+    { text: 'Have Fun', key: '3' }
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+
+    if (text.length > 3) {
+
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOPS', 'Todos Must Be Over 3 Chars Long', [
+        { text: 'Understood', onPress: () => console.log('Alert Closed') }
+      ])
+
+    }
+
+
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -15,7 +64,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
+
 });
